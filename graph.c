faad81f86d4dc0d8df4ea058e83835260607c5f2
Ayoub <ayoub.elboukhari.off@gmail.com>
2026-09-07 09:32:51 +0800

    implement add_friendship and display, working graph

--- graph.c
+++ graph.c
@@ -55,7 +55,54 @@ printf("the user dosn t exist ");
 return -1;
 }   
 int remove_user(Graph *g,int idx);
-int add_friendship(Graph *g, int idx1, int idx2);
+int add_friendship(Graph *g, int idx1, int idx2){
+if(idx1<0 || idx2<0 || idx1>=g->count || idx2>=g->count){
+    printf("invalid index\n");
+    return -1;
+}else if(idx1==idx2){
+    printf("cannot add friendship with self\n");
+    return -1;
+}else if (g->users[idx1].active == 0 || g->users[idx2].active == 0) {
+    printf("users are not active\n");
+    return -1;
+}else if (g->matrix[idx1][idx2] == 1) {
+    printf("already friends\n");
+    return 0;
+}
+EdgeNode *node1 = malloc(sizeof(EdgeNode));
+if (node1 == NULL) {
+    printf("out of memory\n");
+    return 0;
+}
+node1->neighbor = idx2;
+node1->next = g->users[idx1].head;
+g->users[idx1].head = node1;
+EdgeNode *node2 = malloc(sizeof(EdgeNode));
+if (node2 == NULL) {
+    printf("out of memory\n");
+   
+    return 0;
+}
+node2->neighbor = idx1;
+node2->next = g->users[idx2].head;
+g->users[idx2].head = node2;
+g->matrix[idx1][idx2] = 1;
+g->matrix[idx2][idx1] = 1;
+return 1;
+}
+
 int remove_friendship(Graph *g, int idx1, int idx2);
-void display(Graph *g);
+void display(Graph *g){
+    for(int i=0;i<g->count;i++){
+         if (!g->users[i].active) continue;
+        if(g->users[i].active == 1){
+            printf("user %s index %d\n",g->users[i].name,i);
+        }
+        EdgeNode *curr = g->users[i].head;
+        while(curr != NULL){
+            printf("friend index %d and name %s\n",curr->neighbor,g->users[curr->neighbor].name );
+            curr = curr->next;
+        }
+    }
+}
 void free_graph(Graph *g);
--- main.c
+++ main.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 #include "graph.h"
 
+#include <stdio.h>
+#include "graph.h"
+
 int main(void)
 {
     Graph g;
     graph_init(&g);
 
-    printf("%d\n", add_user(&g, "Ayoub"));
-    printf("%d\n", add_user(&g, "Ismail"));
-    printf("%d\n", add_user(&g, "Ayoub"));   /* should be -1 */
-    printf("%d\n", find_user(&g, "Ismail")); /* should be 1 */
-    printf("%d\n", find_user(&g, "Omar"));   /* should be -1 */
+    int a = add_user(&g, "Ayoub");
+    int i = add_user(&g, "Ismail");
+    int o = add_user(&g, "Omar");
+
+    add_friendship(&g, a, i);
+    add_friendship(&g, a, o);
+
+    display(&g);
 
     return 0;
 }
